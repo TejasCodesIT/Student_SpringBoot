81ef403e2bd96b803a656c30bf0f22d1708d3496
Wakchaure123 <133526230+Wakchaure123@users.noreply.github.com>
2024-11-18 16:08:43 +0530

    Project created

---
+++ src/test/java/com/tejas/SpringMVCBoot/StudentsSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tejas.SpringMVCBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
